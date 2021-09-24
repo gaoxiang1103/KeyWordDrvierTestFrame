d511b27094d61807e048becd75ea26cab4601037
gaoxiang1103 <63225237+gaoxiang1103@users.noreply.github.com>
2021-09-24 14:43:07 +0800

    Delete 21Days directory

--- 21Days/正则表达式.py
+++
@@ -1,6 +0,0 @@
-import re
-
-print(re.compile(r'123').findall('4567123789'))
-#
-# regex = re.compile(r'l')
-# regex.findall('hello')
--- 21Days/递归函数.py
+++
@@ -1,9 +0,0 @@
-def fuc(num):
-    result = 1
-    if num == 1:
-        return 1
-    result = num * fuc(num - 1)
-    return result
-
-
-print(fuc(5))
