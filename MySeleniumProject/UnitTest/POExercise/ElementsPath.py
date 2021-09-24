78c8c62f7a4535c3f1a141fee0e60d8aba0495cf
gaoxiang1103 <63225237+gaoxiang1103@users.noreply.github.com>
2021-09-24 15:29:34 +0800

    Delete MySeleniumProject directory

--- MySeleniumProject/UnitTest/POExercise/ElementsPath.py
+++
@@ -1,8 +0,0 @@
-#对元素进行定位
-from selenium.webdriver.common.by import By
-
-class ElementsPath():
-    def __init__(self):
-        self.username = (By.XPATH, "//*[@id='username']")
-        self.password = (By.XPATH, "//*[@id='password']")
-        self.submit = (By.XPATH, "//*[@id='tijiao']")
--- MySeleniumProject/UnitTest/Test/TestCase.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestCase(unittest.TestCase):
-    def setUp(self):
-        print("开始测试了")
-
-    def tearDown(self):
-        print("测试结束了")
